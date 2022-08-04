71eae435d363b67a1c92e0378ee60eff64bbdd12
LucasGomezOliva <lucasgomezoliva@gmail.com>
2022-08-04 17:06:03 -0300

    Agregar archivos de proyecto.

---
+++ Ejercicio-01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint numero = 2;
+            Console.WriteLine("Numero:" + numero);
+            Console.WriteLine("Cantidad de digitos:" + CantidadDigitos(numero));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="numero"></param>
+        /// <returns></returns>
+        static uint CantidadDigitos(uint numero)
+        {
+            if(numero < 10) { return 1; }
+
+            else
+            {
+                return 1 + CantidadDigitos(numero / 10);
+            }
+        }
+    }
+}
---
+++ Ejercicio-02/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            InvertirNoRecursivo();
+            InvertirNoRecursivo();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static void InvertirRecursivo()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        static void InvertirNoRecursivo()
+        {
+
+        }
+    }
+}
