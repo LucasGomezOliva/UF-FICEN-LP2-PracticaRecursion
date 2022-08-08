e9c43587c00abb76fb6bbb73263069f924f9e2bf
LucasGomezOliva <lucasgomezoliva@gmail.com>
2022-08-08 17:00:18 -0300

    Ejercicio-07-08

--- Ejercicio-07/Program.cs
+++ Ejercicio-07/Program.cs
@@ -10,6 +10,25 @@ namespace Ejercicio_07
     {
         static void Main(string[] args)
         {
+            int[] array = { 1, 2, 3};
+
+            Console.WriteLine(SumaArray(array, array.Length - 1));
+
+        }
+
+        /// <summary>
+        /// Realiza la suma de lementos del array
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        static int SumaArray(int[] array,int index)
+        {
+            if(index<0){return 0; }
+            else
+            {
+                return array[index] + SumaArray(array, index - 1);
+            }
         }
     }
 }
--- Ejercicio-08/Program.cs
+++ Ejercicio-08/Program.cs
@@ -10,6 +10,33 @@ namespace Ejercicio_08
     {
         static void Main(string[] args)
         {
+            uint A_n = 5;
+            uint A_r = 3;
+
+            Console.WriteLine("C({0},{1}) = " + Combinatorio(A_n, A_r), A_n, A_r);
+
+            uint B_n = 9;
+            uint B_r = 4;
+
+            Console.WriteLine("C({0},{1}) = " + Combinatorio(B_n, B_r), B_n, B_r);
+        }
+
+        /// <summary>
+        /// Las combinaciones de n elementos tomados de r en r
+        /// </summary>
+        /// <param name="n">Conjunto n</param>
+        /// <param name="r">Elementos r</param>
+        /// <returns>Combinaciones</returns>
+        static uint Combinatorio(uint n, uint r)
+        {
+            if (r == 0) { return 1; }
+
+            if (n == 0) { return 0; }
+
+            else
+            {
+                return Combinatorio(n - 1, r - 1) + Combinatorio(n - 1, r);
+            }
         }
     }
 }
